69f2292087c697348f2359ec8c2ee10d1ba6f722
Armaan Puri <armaanpuri17@gmail.com>
2021-03-13 19:33:09 +0530

    Setup.sh and image files

---
+++ setup.sh
@@ -0,0 +1,13 @@
+# put weights in repo working directory
+! mkdir ./incubit_data
+! mkdir ./incubit_data/train ./incubit_data/trainannot
+! mkdir ./incubit_data/test ./incubit_data/testannot
+! mkdir ./incubit_data/val ./incubit_data/valannot
+
+! cp ./drive/MyDrive/incubit/raw/*.png ./incubit_data/train/
+! cp ./drive/MyDrive/incubit/annotation_masks/* ./incubit_data/trainannot/
+
+! mv ./incubit_data/train/6_6.png ./incubit_data/train/6_7.png ./incubit_data/train/6_8.png ./incubit_data/train/7_6.png ./incubit_data/train/7_7.png ./incubit_data/train/7_8.png ./incubit_data/train/8_6.png ./incubit_data/train/8_7.png ./incubit_data/train/8_8.png -t ./incubit_data/test
+
+! mv ./incubit_data/train/0_6.png ./incubit_data/train/1_7.png ./incubit_data/train/2_8.png ./incubit_data/train/3_6.png ./incubit_data/train/4_7.png -t ./incubit_data/val
+! mv ./incubit_data/trainannot/0_6.png ./incubit_data/trainannot/1_7.png ./incubit_data/trainannot/2_8.png ./incubit_data/trainannot/3_6.png ./incubit_data/trainannot/4_7.png -t ./incubit_data/valannot
